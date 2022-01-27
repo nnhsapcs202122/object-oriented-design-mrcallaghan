e03f6e68d55a9dff901bb015117fe4ae651e17b0
Matt Callaghan <mcallaghan@naperville203.org>
2022-01-27 10:58:09 -0600

    done through peer instruction 1 & 2

--- FillInQuestion.java
+++ FillInQuestion.java
@@ -18,7 +18,6 @@ public class FillInQuestion extends Question
      *   are inherited from the Question class.
      */
     
-    
     /**
      * Constructs a FillInQuestion object with the specified text that contains the answer.
      */
@@ -33,6 +32,48 @@ public class FillInQuestion extends Question
          *     call the superclass's default (i.e., no parameters) constructor.
          */
         super(question);
+    }
+    
+    /**
+     * This method overrides the setText method in the Question class.
+     * 
+     * Sets the text and the answer.
+     * 
+     * @param questionText the text of the question, including the answer.
+     */
+    @Override
+    public void setText(String questionText)
+    {
+        Scanner parser = new Scanner(questionText);
+        parser.useDelimiter("_");
+        String question = parser.next();  // "The inventor of Java is "
+        String answer = parser.next();          // "James Gosling"
+        question += "________" + parser.next(); // "________."
+        
+        
+        /*
+         * The inherited instance variables are private; they cannot be directly accessed.
+         *   We must use the mutator and accessor methods.
+         */
+        //this.text = question;
+        //this.answer = answer;
+        
+        /*
+         * Use the "super" reserved word to call the setText method as defined in the superclass 
+         *   (e.g. Question)
+         */
+        super.setText(question);
+        
+        /*
+         * We should use the "this" reserved word to call the setAnswer method.  If
+         *   the subclass doesn't overrride this method, the superclass's method will be called.
+         *   
+         *   We don't want to use "super" in this case because if we later override setAnswer, 
+         *   the overridden method would not be called.
+         */
+        this.setAnswer(answer);
+    
+    
     }
 
 }
